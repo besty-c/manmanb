8967ebd139b177339e7aa5f22df4dcbbe717db22
besty-c <1095032457@qq.com>
2019-02-19 21:35:30 +0800

    第一次提交

---
+++ js/index.js
@@ -0,0 +1,59 @@
+$(function(){
+    // 轮播图初始化
+    var gallery = mui('.mui-slider');
+    gallery.slider({
+    interval:5000//自动轮播周期，若为0则不自动播放，默认为0；
+    });
+    // nav
+    $.ajax({
+        url:"http://localhost:9090/api/getindexmenu",
+        datatype:"json",
+        success:function(obj){
+            // var attr = [];
+            // for(var i = 0 ; i < obj.result.length;i++){
+            //   obj.result[7].titlehref.replace('more.html','javascript:void(0)')
+            //   attr += obj.result[i];
+            // }
+            // console.log(attr);
+            
+            var html = template("nav",obj);
+            $(".content").html(html);
+            
+        }
+    });
+    
+    // $("#number8").hide()
+    //     $("#number9").hide()
+    //     $("#number10").hide()
+    //     $("#number11").hide()
+    // 更多按钮 点击显示第三行导航栏 
+    // 切换类
+    $(".content").on("tap","#number7",function(){
+        
+        $("#number8").toggleClass("hide")
+        $("#number9").toggleClass("hide")
+        $("#number10").toggleClass("hide")
+        $("#number11").toggleClass("hide")
+        // console.log($("#number7").find('a').css()["0"])
+        // 修改了 更多href
+        $("#number7").find('a').css()["0"].href="javascript:void(0)";
+   
+    });
+
+    $.ajax({
+        url:"http://localhost:9090/api/getmoneyctrl",
+        datatype:"json",
+        success:function(obj){
+            console.log(obj);
+            var html = template("reco",obj);
+            $(".discount").html(html);
+            
+        }
+    })
+    
+       
+       
+   
+    
+    
+})
